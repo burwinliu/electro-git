fd3fbeb9460584223cb22feb9eac18a5c8ccfbe8
burwinliu <burwincollege@gmail.com>
2020-07-27 12:10:12 -0700

    Inputting Menu Bar

---
+++ src/features/menuBar/MenuBarStyles.js
@@ -0,0 +1,19 @@
+import {menuHeight} from "../../styles/constants"
+import { colors } from "../../styles/palette"
+
+export const WindowControlsWrapper = {
+    WebkitAppRegion: "no-drag"
+}
+
+export const MenuBarWrapper = {
+    flexDirection: "row",
+    flexGrow: "1",
+    height: menuHeight,
+    maxHeight: menuHeight,
+    justifyContent: "space-between",
+    width: "100vw",
+    backgroundColor: colors.menu,
+
+    WebkitAppRegion: "drag",
+    WebkitUserSelect: "none"
+}
