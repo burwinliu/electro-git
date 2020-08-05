53f1ad24c1613f595f8920de2e27ad6eba6494cf
burwinliu <burwincollege@gmail.com>
2020-08-05 11:49:19 -0700

    test conflicting

--- main.js
+++ main.js
@@ -7,7 +7,8 @@ const url = require('url')
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
-let mainWindow
+// Test conflict
+let mainWindow 
 
 // Keep a reference for dev mode
 let dev = false
